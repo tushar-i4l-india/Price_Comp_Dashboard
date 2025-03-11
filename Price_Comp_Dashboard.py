600946b4457cc9d1cf142377236ddc8c65b880e9
tushar-i4l-india <tushar.insulation4less@gmail.com>
2025-03-11 13:20:30 +0530

    Update for Material Market graph not shown

--- Price_Comp_Dashboard.py
+++ Price_Comp_Dashboard.py
@@ -15,34 +15,12 @@ def load_data(file_path):
     return df
 
 def extract_first_price(price_str):
-    # if pd.isnull(price_str) or not isinstance(price_str, str):
-    #     return None
-    # match = re.search(r'£(\d+\.\d+)', price_str)
-    # if match:
-    #     return float(match.group(1))
-    
-    # return None
-    # return float(match.group(1)) if match else None
-    # if pd.isnull(price_str):
-    #     return None
-    
-    # # Remove any non-numeric characters except '.' and digits
-    # cleaned_str = re.sub(r"[^\d\.]", "", price_str)
-    
-    # # Check if the cleaned string is a valid decimal number
-    # try:
-    #     return float(cleaned_str) if cleaned_str else None
-    # except ValueError:
-    #     return None
     if pd.isnull(price_str):
         return None
-    
     # Find the first valid price (with or without £)
     match = re.search(r'£?(\d+\.\d+)', price_str)
-    
     if match:
         return float(match.group(1))
-    
     return None
 
 # Set the base directory containing brand directories
