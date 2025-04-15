11ef99d6418f38fb0cf7e02751271638518dcae3
tushar-i4l-india <tushar.insulation4less@gmail.com>
2025-04-15 12:55:26 +0530

    Update for image slideshow on idle mode

--- Price_Comp_Dashboard.py
+++ Price_Comp_Dashboard.py
@@ -281,7 +281,7 @@ if st.session_state.selected_brand:
                     }
                     slides[slideIndex-1].style.display = "block";  
                     dots[slideIndex-1].className += " active";
-                    setTimeout(showSlides, 6000); // Change image every 2 seconds
+                    setTimeout(showSlides, 4000); // Change image every 2 seconds
                 }
                 </script>
 
