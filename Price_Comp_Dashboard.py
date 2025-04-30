f1bd60571300d8f1ec1f880196792e4d1201407f
tushar-i4l-india <tushar.insulation4less@gmail.com>
2025-04-30 11:40:22 +0530

    Auto-update: Wed 04/30/2025 11:40:22.24

--- Price_Comp_Dashboard.py
+++ Price_Comp_Dashboard.py
@@ -62,7 +62,7 @@ if 'previous_date_str' not in st.session_state:
 
 st.sidebar.title("Price Comparison Dashboard 💷")
 
-brands = ["Celotex", "Recticel", "Ecotherm", "Unilin"]
+brands = ["Celotex", "Recticel", "Ecotherm", "Unilin", "Novia"]
 st.session_state.selected_brand = st.sidebar.selectbox("Select Brand", brands)
 
 if st.session_state.selected_brand:
