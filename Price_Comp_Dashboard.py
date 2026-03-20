5b447e85295a20c386c7a6d1265b5de217abd8d0
tushar-i4l-india <tushar.insulation4less@gmail.com>
2026-03-20 13:44:54 +0530

    Auto-update: Fri 03/20/2026 13:44:54.20

--- Price_Comp_Dashboard.py
+++ Price_Comp_Dashboard.py
@@ -177,17 +177,22 @@ div[data-testid="stFormSubmitButton"]{
         margin-bottom:20px;
     }
 
-    .logo-container img{
-        width:420px;
-        animation: glow 2.5s infinite alternate;
-    }
+.logo-container img{
+    width:420px;
+    animation: glow 1.2s infinite alternate;   /* faster blinking */
+}
 
-    @keyframes glow{
-        0%{filter: drop-shadow(0px 0px 5px rgba(255,255,255,0.2));}
-        50%{filter: drop-shadow(0px 0px 20px rgba(255,255,255,0.9));}
-        100%{filter: drop-shadow(0px 0px 5px rgba(255,255,255,0.2));}
+@keyframes glow{
+    0%{
+        filter: brightness(1) drop-shadow(0px 0px 5px rgba(255,255,255,0.3));
     }
-
+    50%{
+        filter: brightness(1.6) drop-shadow(0px 0px 25px rgba(255,255,255,1));
+    }
+    100%{
+        filter: brightness(2) drop-shadow(0px 0px 40px rgba(255,255,255,1));
+    }
+}
     .login-title{
         font-size:32px;
         font-weight:bold;
